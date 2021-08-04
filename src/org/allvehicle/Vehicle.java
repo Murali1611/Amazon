eab98b2f2fafa755df59cbab33788289336a6484
Murali1611 <ilarum1611@gmail.com>
2021-08-04 14:11:33 +0530

    First Push

---
+++ src/org/allvehicle/Vehicle.java
@@ -0,0 +1,24 @@
+package org.allvehicle;
+
+import org.fourwheeler.FourWheeler;
+import org.threewheeler.ThreeWheeler;
+import org.twowheeler.TwoWheller;
+
+public class Vehicle {
+	private void vehicleNecessery() {
+		System.out.println("Vehicles Necessary : Yes or No");
+	}
+	public static void main(String[] args) {
+		Vehicle a = new Vehicle();
+		a.vehicleNecessery(); 
+		TwoWheller b = new TwoWheller();
+		b.bike();
+		b.cycle();
+		ThreeWheeler c = new ThreeWheeler();
+		c.auto();
+		FourWheeler d = new FourWheeler();
+		d.bus();
+		d.car();
+		d.lorry();
+	}
+}
